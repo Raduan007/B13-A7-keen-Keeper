24f888b2caeef51e94b6876159438f19b0c578d0
raduan007 <raduanhossen517@gmail.com>
2026-04-15 13:03:38 +0600

    Timeline Filter Added

--- src/pages/timelinepage/TimelinePage.jsx
+++ src/pages/timelinepage/TimelinePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoVideocamOutline } from "react-icons/io5";
 import { MdOutlineTextsms, MdOutlineWifiCalling3 } from "react-icons/md";
 import { useCheckIn } from "../../ui/CheckInContext";
@@ -6,11 +6,11 @@ import { useCheckIn } from "../../ui/CheckInContext";
 const getIcon = (type) => {
   switch (type) {
     case "call":
-      return <MdOutlineWifiCalling3 size={30}  />;
+      return <MdOutlineWifiCalling3 size={22} />;
     case "text":
-      return <MdOutlineTextsms  size={30} />;
+      return <MdOutlineTextsms size={22} />;
     case "video":
-      return <IoVideocamOutline size={30}  />;
+      return <IoVideocamOutline size={22} />;
     default:
       return null;
   }
@@ -19,6 +19,15 @@ const getIcon = (type) => {
 const TimelinePage = () => {
   const { timeline } = useCheckIn();
 
+
+  const [filter, setFilter] = useState("all");
+
+
+  const filteredTimeline =
+    filter === "all"
+      ? timeline
+      : timeline.filter((item) => item.type === filter);
+
   return (
     <div className="max-w-xl mx-auto p-4">
 
@@ -26,15 +35,33 @@ const TimelinePage = () => {
         Timeline
       </h2>
 
+
+      <div className="mb-4">
+
+        <select
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          className=" w-1/2 p-3 shadow rounded-lg text-gray-600 bg-gray-100">
+
+          <option value="all">Filter timeline</option>
+           <option value="text">Text</option>
+          <option value="call">Call</option>
+          <option value="video">Video</option>
+        </select>
+
+      </div> 
+
+   
+
       <div className="space-y-3">
 
-        {timeline.length === 0 && (
+        {filteredTimeline.length === 0 && (
           <div className="text-gray-500">
             No activities yet
           </div>
         )}
 
-        {timeline.map((item) => (
+        {filteredTimeline.map((item) => (
           <div
             key={item.id}
             className="bg-white rounded-lg shadow-md p-4 flex items-start gap-3"
@@ -45,7 +72,7 @@ const TimelinePage = () => {
 
             <div>
               <p className="font-medium text-gray-800">
-                {item.type} with {item.contactName}
+                {item.type} with {item.contactName || "Friend"}
               </p>
 
               <p className="text-sm text-gray-500">
