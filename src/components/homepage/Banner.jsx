fc4059c78dc6e5f8b97e2c5f92af841308d01270
raduan007 <raduanhossen517@gmail.com>
2026-04-16 14:56:50 +0600

    Font-Famil Added

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -4,7 +4,7 @@ import { FaPlus } from 'react-icons/fa';
 const Banner = () => {
     return (
         <div className='text-center'>
-            <h1 className='text-4xl pt-16 pb-4'>Friends to keep close in your life</h1>
+            <h1 className='text-5xl pt-16 pb-4'>Friends to keep close in your life</h1>
             <p className='opacity-80 pb-4'>Your personal shelf of meaningful connections. Browse, tend, and nurture the <br />
              relationships that matter most.</p>
              <div className='flex justify-center '>
